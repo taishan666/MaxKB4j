df1fc5698a309b05fb2508a0812f9fc85af2b97c
tarzan <1334512682@qq.com>
2025-03-27 09:59:52 +0800

    feat(core): 增加集合类型比较支持
    - 在 GTCompare、LECompare 和 LTCompare 类中添加对集合类型的支持
    - 当源值为集合时，比较其大小与目标值
    - 优化了比较逻辑，提高了代码的通用性和灵活性

--- src/main/java/com/tarzan/maxkb4j/core/workflow/node/condition/compare/impl/GTCompare.java
+++ src/main/java/com/tarzan/maxkb4j/core/workflow/node/condition/compare/impl/GTCompare.java
@@ -2,6 +2,8 @@ package com.tarzan.maxkb4j.core.workflow.node.condition.compare.impl;
 
 import com.tarzan.maxkb4j.core.workflow.node.condition.compare.Compare;
 
+import java.util.Collection;
+
 public class GTCompare implements Compare {
     @Override
     public boolean support(String compare) {
@@ -10,6 +12,9 @@ public class GTCompare implements Compare {
 
     @Override
     public boolean compare(Object sourceValue,  String targetValue) {
+        if (sourceValue instanceof Collection){
+            return ((Collection<?>) sourceValue).size()> Integer.parseInt(targetValue);
+        }
         return (float) sourceValue> Float.parseFloat(targetValue);
     }
 }
--- src/main/java/com/tarzan/maxkb4j/core/workflow/node/condition/compare/impl/LTCompare.java
+++ src/main/java/com/tarzan/maxkb4j/core/workflow/node/condition/compare/impl/LTCompare.java
@@ -2,6 +2,8 @@ package com.tarzan.maxkb4j.core.workflow.node.condition.compare.impl;
 
 import com.tarzan.maxkb4j.core.workflow.node.condition.compare.Compare;
 
+import java.util.Collection;
+
 public class LTCompare implements Compare {
     @Override
     public boolean support(String compare) {
@@ -10,6 +12,9 @@ public class LTCompare implements Compare {
 
     @Override
     public boolean compare(Object sourceValue,  String targetValue) {
-        return (float) sourceValue <= Float.parseFloat(targetValue);
+        if (sourceValue instanceof Collection){
+            return ((Collection<?>) sourceValue).size()< Integer.parseInt(targetValue);
+        }
+        return (float) sourceValue < Float.parseFloat(targetValue);
     }
 }
