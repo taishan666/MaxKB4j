5cbb09cec98568c9a815927bccdcd2049c3b73f3
tarzan <1334512682@qq.com>
2025-03-31 11:21:42 +0800

    feat(core): 实现 BaseMcpNode 并更新依赖版本
    - 新增 McpClient 和 ToolProvider 的配置
    - 实现 getDetail 方法返回节点详情
    - 更新 pom.xml 中多个依赖的版本

--- src/main/java/com/tarzan/maxkb4j/core/workflow/node/mcp/impl/BaseMcpNode.java
+++ src/main/java/com/tarzan/maxkb4j/core/workflow/node/mcp/impl/BaseMcpNode.java
@@ -6,16 +6,44 @@ import com.tarzan.maxkb4j.core.workflow.WorkflowManage;
 import com.tarzan.maxkb4j.core.workflow.node.mcp.IMcpNode;
 import com.tarzan.maxkb4j.core.workflow.node.mcp.input.McpParams;
 import com.tarzan.maxkb4j.core.workflow.node.start.input.FlowParams;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.mcp.McpToolProvider;
+import dev.langchain4j.mcp.client.DefaultMcpClient;
+import dev.langchain4j.mcp.client.McpClient;
+import dev.langchain4j.mcp.client.transport.McpTransport;
+import dev.langchain4j.mcp.client.transport.http.HttpMcpTransport;
+import dev.langchain4j.service.tool.ToolProvider;
+
+import java.util.List;
 
 public class BaseMcpNode extends IMcpNode {
     @Override
     public NodeResult execute(McpParams nodeParams, FlowParams workflowParams) {
+        McpTransport transport = new HttpMcpTransport.Builder()
+                .sseUrl("http://localhost:3001/sse")
+                .logRequests(true) // if you want to see the traffic in the log
+                .logResponses(true)
+                .build();
+        McpClient mcpClient = new DefaultMcpClient.Builder()
+                .transport(transport)
+                .build();
+        ToolProvider toolProvider = McpToolProvider.builder()
+                .mcpClients(List.of(mcpClient))
+                .build();
         return null;
     }
 
     @Override
     public JSONObject getDetail() {
-        return null;
+        JSONObject detail = new JSONObject();
+        detail.put("system", context.get("system"));
+        detail.put("question", context.get("question"));
+        List<ChatMessage> historyMessage = (List<ChatMessage>) context.get("history_message");
+        detail.put("history_message", resetMessageList(historyMessage));
+        detail.put("answer", context.get("answer"));
+        detail.put("messageTokens", context.get("messageTokens"));
+        detail.put("answerTokens", context.get("answerTokens"));
+        return detail;
     }
 
     @Override
