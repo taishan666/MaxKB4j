bb0ec77bead21420786b7f08ac163fa148523846
tarzan <1334512682@qq.com>
2025-04-21 13:21:28 +0800

    refactor(core): 优化数据处理和工作流配置
    - 将 StringArrayTypeHandler 替换为 JOSNBListTypeHandler，以提高数据处理效率
    - 移除工作流配置中的示例数据集和模型 ID，简化配置结构

--- src/main/java/com/tarzan/maxkb4j/module/application/entity/ApplicationChatRecordEntity.java
+++ src/main/java/com/tarzan/maxkb4j/module/application/entity/ApplicationChatRecordEntity.java
@@ -5,8 +5,8 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.tarzan.maxkb4j.core.common.entity.BaseEntity;
+import com.tarzan.maxkb4j.core.handler.type.JOSNBListTypeHandler;
 import com.tarzan.maxkb4j.core.handler.type.JOSNBTypeHandler;
-import com.tarzan.maxkb4j.core.handler.type.StringArrayTypeHandler;
 import com.tarzan.maxkb4j.core.handler.type.StringSetTypeHandler;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -36,7 +36,7 @@ public class ApplicationChatRecordEntity extends BaseEntity {
 	private Float runTime;
 	private Integer index;
 	private String chatId;
-	@TableField(typeHandler = StringArrayTypeHandler.class)
+	@TableField(typeHandler = JOSNBListTypeHandler.class)
 	private List<String> answerTextList;
 
     public JSONObject getNodeDetailsByRuntimeNodeId(String runtimeNodeId) {
