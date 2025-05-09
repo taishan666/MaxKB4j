2210ddf7951e5dd0fe48bf7a1611dee5a6db7f29
tarzan <1334512682@qq.com>
2025-05-09 16:28:54 +0800

    feat(core): 实现 MCP节点通用执行逻辑
    - 新增 McpParams 类用于 MCP节点参数配置
    -重构 BaseMcpNode 类，支持从节点参数中获取 SSE URL- 添加工具规范和执行请求的构建逻辑
    - 实现 MCP客户端初始化和工具执行

--- src/main/java/com/tarzan/maxkb4j/core/workflow/node/mcp/impl/BaseMcpNode.java
+++ src/main/java/com/tarzan/maxkb4j/core/workflow/node/mcp/impl/BaseMcpNode.java
@@ -3,14 +3,14 @@ package com.tarzan.maxkb4j.core.workflow.node.mcp.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.tarzan.maxkb4j.core.workflow.INode;
 import com.tarzan.maxkb4j.core.workflow.NodeResult;
-import dev.langchain4j.mcp.McpToolProvider;
+import com.tarzan.maxkb4j.core.workflow.node.mcp.input.McpParams;
+import dev.langchain4j.agent.tool.ToolExecutionRequest;
+import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.mcp.client.DefaultMcpClient;
 import dev.langchain4j.mcp.client.McpClient;
 import dev.langchain4j.mcp.client.transport.McpTransport;
 import dev.langchain4j.mcp.client.transport.http.HttpMcpTransport;
-import dev.langchain4j.service.tool.ToolProvider;
 
-import java.util.List;
 import java.util.Map;
 
 import static com.tarzan.maxkb4j.core.workflow.enums.NodeType.MCP;
@@ -24,18 +24,22 @@ public class BaseMcpNode extends INode {
 
     @Override
     public NodeResult execute() {
+        McpParams nodeParams=super.nodeParams.toJavaObject(McpParams.class);
+        JSONObject params=new JSONObject();
         McpTransport transport = new HttpMcpTransport.Builder()
-                .sseUrl("http://localhost:3001/sse")
-                .logRequests(true) // if you want to see the traffic in the log
-                .logResponses(true)
+                .sseUrl(nodeParams.getSseUrl())
                 .build();
         McpClient mcpClient = new DefaultMcpClient.Builder()
                 .transport(transport)
                 .build();
-        ToolProvider toolProvider = McpToolProvider.builder()
-                .mcpClients(List.of(mcpClient))
+        ToolSpecification toolSpecification=mcpClient.listTools().get(0);
+        ToolExecutionRequest toolExecutionRequest=ToolExecutionRequest.builder()
+                .id("1")
+                .name(toolSpecification.name())
+                .arguments(params.toJSONString())
                 .build();
-        return new NodeResult(Map.of("result",context.get("result")), Map.of());
+        String result=mcpClient.executeTool(toolExecutionRequest);
+        return new NodeResult(Map.of("result",result), Map.of());
     }
 
     @Override
--- src/main/java/com/tarzan/maxkb4j/core/workflow/node/mcp/input/McpParams.java
+++ src/main/java/com/tarzan/maxkb4j/core/workflow/node/mcp/input/McpParams.java
@@ -1,8 +1,9 @@
 package com.tarzan.maxkb4j.core.workflow.node.mcp.input;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
 public class McpParams {
+
+    private String sseUrl;
 }
