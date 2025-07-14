31be47ffa75094aaf2948ce576a45ea647234ea6
tarzan <1334512682@qq.com>
2025-07-14 08:51:25 +0800

    refactor(module): 优化 IBaseChatPipelineStep 类
    - 删除了未使用的 validArgs 方法注释- 移除了 run 方法中的无效代码
    - 简化了 run 方法的实现，提高了代码可读性

--- src/main/java/com/tarzan/maxkb4j/module/application/ragpipeline/IBaseChatPipelineStep.java
+++ src/main/java/com/tarzan/maxkb4j/module/application/ragpipeline/IBaseChatPipelineStep.java
@@ -5,12 +5,9 @@ import com.alibaba.fastjson.JSONObject;
 public abstract class IBaseChatPipelineStep {
     protected JSONObject context = new JSONObject();
 
-   // public abstract void validArgs(PipelineManage manage);
-
     public void run(PipelineManage manage) {
         long startTime = System.currentTimeMillis();
         context.put("start_time", startTime);
-      //  validArgs(manage);
         _run(manage);
         context.put("runTime", (System.currentTimeMillis()-startTime)/1000F);
     }
