23bd0a0748d492fc6feb5aa27873d944216abe1d
tarzan <1334512682@qq.com>
2025-09-01 17:21:29 +0800

    refactor(module): 优化模型构建逻辑
    - 当指定的 modelName未找到时，改为使用第一个匹配 modelType 的模型
    - 移动了 assert modelInfo != null 的位置，确保在实例化 BaseModel前进行检查

--- src/main/java/com/tarzan/maxkb4j/module/model/provider/IModelProvider.java
+++ src/main/java/com/tarzan/maxkb4j/module/model/provider/IModelProvider.java
@@ -47,9 +47,13 @@ public abstract class IModelProvider {
     <T> T build(String modelName, String modelType, ModelCredential modelCredential, JSONObject params) {
         List<ModelInfo> modelList = getModelList();
         ModelInfo modelInfo = modelList.stream().filter(model -> model.getModelType().equals(modelType) && model.getName().equals(modelName)).findFirst().orElse(null);
-        assert modelInfo != null;
+        if (modelInfo == null){
+            //没有的话，取第一个model的构造器
+            modelInfo = modelList.stream().filter(model -> model.getModelType().equals(modelType)).findFirst().orElse(null);
+        }
         try {
             // 创建 BaseModel 实现类的实例
+            assert modelInfo != null;
             BaseModel<T> instance = (BaseModel<T>) modelInfo.getModelClass().getDeclaredConstructor().newInstance();
             return instance.build(modelName, modelCredential,params);
         } catch (Exception e) {
