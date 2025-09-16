6ed265772a6a5f55273f9b6fb3dd054ed0dac974
tarzan <1334512682@qq.com>
2025-09-16 09:13:38 +0800

    feat(chat): 实现聊天功能基本接口
    - 添加应用配置接口，获取应用信息
    - 实现聊天室开启接口
    - 添加匿名认证接口
    - 实现聊天消息发送和接收功能
    - 引入 Sinks 和 Flux 用于实时消息推送
    - 增加历史会话接口（未实现）

--- src/main/java/com/tarzan/maxkb4j/module/chat/ChatApiController.java
+++ src/main/java/com/tarzan/maxkb4j/module/chat/ChatApiController.java
@@ -1,13 +1,25 @@
 package com.tarzan.maxkb4j.module.chat;
 
+import cn.dev33.satoken.stp.SaLoginModel;
 import cn.dev33.satoken.stp.StpUtil;
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.tarzan.maxkb4j.constant.AppConst;
 import com.tarzan.maxkb4j.core.api.R;
+import com.tarzan.maxkb4j.module.application.domian.dto.ChatMessageDTO;
+import com.tarzan.maxkb4j.module.application.domian.entity.ApplicationAccessTokenEntity;
 import com.tarzan.maxkb4j.module.application.domian.entity.ApplicationChatRecordEntity;
 import com.tarzan.maxkb4j.module.application.domian.entity.ApplicationEntity;
+import com.tarzan.maxkb4j.module.application.domian.vo.ChatMessageVO;
+import com.tarzan.maxkb4j.module.application.enums.AuthType;
+import com.tarzan.maxkb4j.module.application.service.ApplicationAccessTokenService;
+import com.tarzan.maxkb4j.module.application.service.ApplicationChatService;
+import com.tarzan.maxkb4j.module.application.service.ApplicationService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
 
 import java.util.List;
 
@@ -15,6 +27,12 @@ import java.util.List;
 @RequestMapping(AppConst.CHAT_API)
 @AllArgsConstructor
 public class ChatApiController {
+
+    private final ApplicationAccessTokenService accessTokenService;
+    private final ApplicationService applicationService;
+    private final ApplicationChatService chatService;
+    private final TaskExecutor chatTaskExecutor;
+
 
     @GetMapping("/profile")
     public R<JSONObject> profile(String accessToken) {
@@ -25,25 +43,45 @@ public class ChatApiController {
 
     @GetMapping("/application/profile")
     public R<ApplicationEntity> appProfile() {
-        return R.success(new ApplicationEntity());
+        String appId = (String) StpUtil.getExtra("application_id");
+        return R.success(applicationService.getById(appId));
     }
 
-    //todo
     @GetMapping("/open")
     public R<String> chatOpen() {
-        return R.success("123456789");
+        String appId = (String) StpUtil.getExtra("application_id");
+        return R.success(chatService.chatOpen(appId));
     }
 
     //todo
     @PostMapping("/auth/anonymous")
     public R<String> auth(@RequestBody JSONObject params) {
+        ApplicationAccessTokenEntity accessToken = accessTokenService.getByToken(params.getString("accessToken"));
+        SaLoginModel loginModel = new SaLoginModel();
+        loginModel.setExtra("username", "anonymous");
+        loginModel.setExtra("language", "");
+        loginModel.setExtra("client_id", IdWorker.get32UUID());
+        loginModel.setExtra("client_type", AuthType.ACCESS_TOKEN.name());
+        loginModel.setExtra("application_id", accessToken.getApplicationId());
+        loginModel.setExtra(AuthType.ACCESS_TOKEN.name(), accessToken);
+        loginModel.setDevice(AuthType.ACCESS_TOKEN.name());
+        StpUtil.login(StpUtil.getLoginId(), loginModel);
         return R.success(StpUtil.getTokenValue());
     }
 
     //todo
     @PostMapping("/chat_message/{chatId}")
-    public R<String> auth(@PathVariable String chatId, @RequestBody JSONObject params) {
-        return R.success(StpUtil.getTokenValue());
+    public Flux<ChatMessageVO> chatMessage(@PathVariable String chatId, @RequestBody ChatMessageDTO params) {
+        Sinks.Many<ChatMessageVO> sink = Sinks.many().multicast().onBackpressureBuffer();
+        String clientId = (String) StpUtil.getExtra("client_id");
+        String clientType = (String) StpUtil.getExtra("client_type");
+        params.setChatId(chatId);
+        params.setClientId(clientId);
+        params.setClientType(clientType);
+        params.setSink(sink);
+        // 异步执行业务逻辑
+        chatTaskExecutor.execute(() -> chatService.chatMessage(params));
+        return sink.asFlux();
     }
 
     @GetMapping("/historical_conversation/{current}/{size}")
