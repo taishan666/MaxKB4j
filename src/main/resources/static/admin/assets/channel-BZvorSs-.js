d04fca9ff019c6f58216ab620736f5eb14736486
tarzan <1334512682@qq.com>
2025-12-10 16:07:45 +0800

    解决前端bug

--- src/main/resources/static/admin/assets/channel-BZvorSs-.js
+++
@@ -1,1 +0,0 @@
-import{aC as o,aD as n}from"./admin-BYKfadXe.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
--- src/main/resources/static/admin/assets/channel-yt6Fy3ve.js
+++
@@ -1,1 +0,0 @@
-import{aC as o,aD as n}from"./admin-BK8mZVyy.js";const t=(a,r)=>o.lang.round(n.parse(a)[r]);export{t as c};
