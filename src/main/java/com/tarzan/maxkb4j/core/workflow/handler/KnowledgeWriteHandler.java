efe146825f69d4aa5714b806f012d8a71e151bcb
startewho <hoolooday@live.com>
2025-12-24 17:14:41 +0800

    fix(workflow): 修复文档分割和知识写入处理逻辑
    - 在文档分割处理器中为段落添加空标题并设置源文件ID
    - 在知识写入处理器中添加调试模式检查避免调试时创建文档
    - 移除变量聚合节点中的调试打印语句

---
+++ src/main/java/com/tarzan/maxkb4j/core/workflow/handler/KnowledgeWriteHandler.java
@@ -0,0 +1,44 @@
+package com.tarzan.maxkb4j.core.workflow.handler.node.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.tarzan.maxkb4j.core.workflow.annotation.NodeHandlerType;
+import com.tarzan.maxkb4j.core.workflow.enums.NodeType;
+import com.tarzan.maxkb4j.core.workflow.handler.node.INodeHandler;
+import com.tarzan.maxkb4j.core.workflow.model.KnowledgeWorkflow;
+import com.tarzan.maxkb4j.core.workflow.model.NodeResult;
+import com.tarzan.maxkb4j.core.workflow.model.Workflow;
+import com.tarzan.maxkb4j.core.workflow.node.INode;
+import com.tarzan.maxkb4j.core.workflow.node.impl.KnowledgeWriteNode;
+import com.tarzan.maxkb4j.module.knowledge.domain.dto.DocumentSimple;
+import com.tarzan.maxkb4j.module.knowledge.service.DocumentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+@NodeHandlerType(NodeType.KNOWLEDGE_WRITE)
+@RequiredArgsConstructor
+public class KnowledgeWriteHandler implements INodeHandler {
+    private final DocumentService documentService;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public NodeResult execute(Workflow workflow, INode node) throws Exception {
+        KnowledgeWriteNode.NodeParams nodeParams = node.getNodeData().toJavaObject(KnowledgeWriteNode.NodeParams.class);
+        Object value = workflow.getReferenceField(nodeParams.getDocumentList().get(0),nodeParams.getDocumentList().get(1));
+        node.getDetail().put("write_content", JSON.toJSON(value));
+        if (workflow instanceof KnowledgeWorkflow knowledgeWorkflow) {
+            boolean debug = knowledgeWorkflow.getKnowledgeParams().isDebug();
+            if (!debug){
+                String knowledgeId = knowledgeWorkflow.getKnowledgeParams().getKnowledgeId();
+                List<DocumentSimple> docs=(List<DocumentSimple>)value;
+                documentService.batchCreateDoc(knowledgeId, docs);
+            }
+        }
+        return new NodeResult(Map.of());
+    }
+}
---
+++ src/main/java/com/tarzan/maxkb4j/core/workflow/handler/VariableAggregationNodeHandler.java
@@ -0,0 +1,69 @@
+package com.tarzan.maxkb4j.core.workflow.handler.node.impl;
+
+import com.tarzan.maxkb4j.core.workflow.annotation.NodeHandlerType;
+import com.tarzan.maxkb4j.core.workflow.enums.NodeType;
+import com.tarzan.maxkb4j.core.workflow.handler.node.INodeHandler;
+import com.tarzan.maxkb4j.core.workflow.model.NodeResult;
+import com.tarzan.maxkb4j.core.workflow.model.Workflow;
+import com.tarzan.maxkb4j.core.workflow.node.INode;
+import com.tarzan.maxkb4j.core.workflow.node.impl.VariableAggregationNode;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@NodeHandlerType(NodeType.VARIABLE_AGGREGATE)
+@Component
+public class VariableAggregationNodeHandler implements INodeHandler {
+    static Map<String, StrategyFunction> strategy_map = new HashMap<>();
+
+    static {
+        strategy_map.put("first_non_null", VariableAggregationNodeHandler::getFirstNonNull);
+        strategy_map.put("variable_to_json", VariableAggregationNodeHandler::getCollection);
+    }
+
+    @Override
+    public NodeResult execute(Workflow workflow, INode node) throws Exception {
+        VariableAggregationNode.NodeParams nodeParams = node.getNodeData().toJavaObject(VariableAggregationNode.NodeParams.class);
+        String strategyName = nodeParams.getStrategy();
+        Map<String, Object> nodeVariable = new HashMap<>();
+        List<VariableAggregationNode.Group> groupList = nodeParams.getGroupList();
+        for (VariableAggregationNode.Group group : groupList) {
+            List<VariableAggregationNode.Variable> variableList = group.getVariableList();
+            resetVariable(variableList,workflow);
+            StrategyFunction strategy = strategy_map.get(strategyName);
+            group.setValue(strategy.apply(variableList));
+            nodeVariable.put(group.getField(), group.getValue());
+        }
+        node.getDetail().put("strategy", strategyName);
+        node.getDetail().put("groupList", groupList);
+        System.out.println(groupList);
+        return new NodeResult(nodeVariable);
+    }
+
+    private void resetVariable(List<VariableAggregationNode.Variable> variableList, Workflow workflow) {
+        for (VariableAggregationNode.Variable e : variableList) {
+            String nodeId = e.getVariable().get(0);
+            String field = e.getVariable().get(1);
+            INode lfNode = workflow.getNode(nodeId);
+            Object value = workflow.getReferenceField(nodeId, field);
+            e.setNodeName(lfNode.getProperties().getString("nodeName"));
+            e.setField(field);
+            e.setValue(value);
+        }
+    }
+
+
+    @FunctionalInterface
+    public interface StrategyFunction {
+        Object apply(List<VariableAggregationNode.Variable> variableList);
+    }
+
+    public static Object getFirstNonNull(List<VariableAggregationNode.Variable> variableList) {
+        return variableList.stream().map(VariableAggregationNode.Variable::getValue).filter(Objects::nonNull).findFirst().orElse(null);
+    }
+
+    public static Object getCollection(List<VariableAggregationNode.Variable> variableList) {
+        return variableList.stream().map(VariableAggregationNode.Variable::getValue).toList();
+    }
+
+}
