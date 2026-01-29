a21da2dcb4ed4753c2459961681e048d47f16b81
tarzan <1334512682@qq.com>
2026-01-29 13:30:19 +0800

    feat(user): 更新用户控制器配置默认密码属性
    - 移除 StpKit 导入并添加 SystemProperties 依赖注入
    - 注入 systemProperties 用于获取系统配置
    - 将硬编码密码 "123456" 替换为从系统属性获取的默认密码
    - 优化代码格式化和空格处理

--- src/main/java/com/tarzan/maxkb4j/module/system/user/controller/UserController.java
+++ src/main/java/com/tarzan/maxkb4j/module/system/user/controller/UserController.java
@@ -5,13 +5,12 @@ import cn.dev33.satoken.annotation.SaMode;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.tarzan.maxkb4j.common.api.R;
 import com.tarzan.maxkb4j.common.constant.AppConst;
-import com.tarzan.maxkb4j.common.util.StpKit;
+import com.tarzan.maxkb4j.common.props.SystemProperties;
 import com.tarzan.maxkb4j.module.system.user.constants.LoginType;
 import com.tarzan.maxkb4j.module.system.user.constants.RoleType;
 import com.tarzan.maxkb4j.module.system.user.domain.dto.PasswordDTO;
 import com.tarzan.maxkb4j.module.system.user.domain.dto.UserDTO;
 import com.tarzan.maxkb4j.module.system.user.domain.entity.UserEntity;
-import com.tarzan.maxkb4j.module.system.user.domain.vo.UserVO;
 import com.tarzan.maxkb4j.module.system.user.service.UserService;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
@@ -27,73 +26,71 @@ import java.util.Map;
 @RestController
 @RequestMapping(AppConst.ADMIN_API)
 @RequiredArgsConstructor
-public class UserController{
+public class UserController {
 
-	private final UserService userService;
+    private final UserService userService;
+	private final SystemProperties systemProperties;
 
+    @SaCheckRole(type = LoginType.ADMIN, value = RoleType.ADMIN)
+    @GetMapping("/user_manage/{page}/{size}")
+    public R<IPage<UserEntity>> userManage(@PathVariable("page") int page, @PathVariable("size") int size, UserDTO dto) {
+        return R.data(userService.selectUserPage(page, size, dto));
+    }
 
+    @SaCheckRole(type = LoginType.ADMIN, value = RoleType.ADMIN)
+    @PostMapping("/user/language")
+    public R<Boolean> language(@RequestBody UserEntity user) {
+        return R.status(userService.updateLanguage(user));
+    }
 
-	@SaCheckRole(type=LoginType.ADMIN,value = RoleType.ADMIN)
-	@GetMapping("/user_manage/{page}/{size}")
-	public R<IPage<UserEntity>> userManage(@PathVariable("page")int page, @PathVariable("size")int size, UserDTO dto){
-		return R.data(userService.selectUserPage(page,size,dto));
-	}
+    @SaCheckRole(type = LoginType.ADMIN, value = RoleType.ADMIN)
+    @PostMapping("/user_manage")
+    public R<Boolean> createUser(@RequestBody UserEntity user) {
+        return R.status(userService.createUser(user));
+    }
 
-	@SaCheckRole(type=LoginType.ADMIN,value = RoleType.ADMIN)
-	@PostMapping("/user/language")
-	public R<Boolean> language(@RequestBody UserEntity user){
-		return R.status(userService.updateLanguage(user));
-	}
+    @SaCheckRole(type = LoginType.ADMIN, value = RoleType.ADMIN)
+    @GetMapping("/user_manage/password")
+    public R<Map<String, String>> password() {
+        return R.data(Map.of("password", systemProperties.getDefaultPassword()));
+    }
 
-	@SaCheckRole(type=LoginType.ADMIN,value = RoleType.ADMIN)
-	@PostMapping("/user_manage")
-	public R<Boolean> createUser(@RequestBody UserEntity user){
-		return R.status(userService.createUser(user));
-	}
+    @SaCheckRole(type = LoginType.ADMIN, value = RoleType.ADMIN)
+    @PutMapping("/user_manage/{id}")
+    public R<Boolean> updateUserById(@PathVariable("id") String id, @RequestBody UserEntity user) {
+        user.setId(id);
+        return R.status(userService.updateById(user));
+    }
 
-	@SaCheckRole(type=LoginType.ADMIN,value = RoleType.ADMIN)
-	@GetMapping("/user_manage/password")
-	public R<Map<String, String>> password(){
-		return R.data(Map.of("password","123456"));
-	}
+    @SaCheckRole(type = LoginType.ADMIN, value = RoleType.ADMIN)
+    @DeleteMapping("/user_manage/{id}")
+    public R<Boolean> deleteUserById(@PathVariable("id") String id) {
+        return R.status(userService.deleteUserById(id));
+    }
 
-	@SaCheckRole(type=LoginType.ADMIN,value = RoleType.ADMIN)
-	@PutMapping("/user_manage/{id}")
-	public R<Boolean> updateUserById(@PathVariable("id")String id,@RequestBody UserEntity user){
-		user.setId(id);
-		return R.status(userService.updateById(user));
-	}
+    @SaCheckRole(type = LoginType.ADMIN, value = RoleType.ADMIN)
+    @PutMapping("/user_manage/{id}/re_password")
+    public R<Boolean> updatePassword(@PathVariable("id") String id, @RequestBody PasswordDTO dto) {
+        if (StringUtils.isBlank(dto.getPassword()) || StringUtils.isBlank(dto.getRePassword())) {
+            return R.fail("密码不能为空");
+        }
+        if (!dto.getPassword().equals(dto.getRePassword())) {
+            return R.fail("密码输入不一致");
+        }
+        return R.status(userService.updatePassword(id, dto));
+    }
 
-	@SaCheckRole(type=LoginType.ADMIN,value = RoleType.ADMIN)
-	@DeleteMapping("/user_manage/{id}")
-	public R<Boolean> deleteUserById(@PathVariable("id")String id){
-		return R.status(userService.deleteUserById(id));
-	}
+    @SaCheckRole(type = LoginType.ADMIN, value = {RoleType.ADMIN, RoleType.USER}, mode = SaMode.OR)
+    @PostMapping("/user/current/send_email")
+    public R<Boolean> sendEmail() throws MessagingException {
+        return R.status(userService.sendEmailCode());
+    }
 
-	@SaCheckRole(type=LoginType.ADMIN,value = RoleType.ADMIN)
-	@PutMapping("/user_manage/{id}/re_password")
-	public R<Boolean> updatePassword(@PathVariable("id")String id,@RequestBody PasswordDTO dto){
-		if (StringUtils.isBlank(dto.getPassword())||StringUtils.isBlank(dto.getRePassword())){
-			return R.fail("密码不能为空");
-		}
-		if(!dto.getPassword().equals(dto.getRePassword())){
-			return R.fail("密码输入不一致");
-		}
-		return R.status(userService.updatePassword(id,dto));
-	}
-
-	@SaCheckRole(type=LoginType.ADMIN,value = {RoleType.ADMIN, RoleType.USER},mode = SaMode.OR)
-	@PostMapping("/user/current/send_email")
-	public R<Boolean> sendEmail() throws MessagingException {
-		return R.status(userService.sendEmailCode());
-	}
-
-	@SaCheckRole(type=LoginType.ADMIN,value = {RoleType.ADMIN, RoleType.USER},mode = SaMode.OR)
-	@PostMapping("/user/current/reset_password")
-	public R<Boolean> resetPassword(@RequestBody PasswordDTO dto) {
-		return R.status(userService.resetPassword(dto));
-	}
-
+    @SaCheckRole(type = LoginType.ADMIN, value = {RoleType.ADMIN, RoleType.USER}, mode = SaMode.OR)
+    @PostMapping("/user/current/reset_password")
+    public R<Boolean> resetPassword(@RequestBody PasswordDTO dto) {
+        return R.status(userService.resetPassword(dto));
+    }
 
 
 }
