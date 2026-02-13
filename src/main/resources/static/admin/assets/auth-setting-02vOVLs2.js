38eb8bf14c39c9ee5d097470963aadbf9355b605
tarzan <1334512682@qq.com>
2026-02-13 11:36:25 +0800

    更新前端资源

--- src/main/resources/static/admin/assets/auth-setting-02vOVLs2.js
+++
@@ -1,1 +0,0 @@
-import{b0 as i,b1 as s,b2 as r}from"./admin-C26zVPd1.js";const n="/auth",u=(t,o)=>r(`${n}/${t}/detail`,void 0,o),a=(t,o)=>i(`${n}/connection`,t,void 0,o),p=(t,o,e)=>s(`${n}/${t}/info`,o,void 0,e),g={getAuthSetting:u,postAuthSetting:a,putAuthSetting:p};export{g as a};
