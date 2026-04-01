3c4a63303493825aa8dee9b86ce7ab398bccc92a
tarzan <1334512682@qq.com>
2026-04-01 13:28:07 +0800

    feat(config): 更新静态资源配置和管理界面
    - 在WebConfig中添加了新的路径模式配置
    - 修改StaticResourceFilter以支持管理员资源路径匹配
    - 重命名管理员界面访问设置相关的JavaScript文件
    - 更新应用访问管理界面的组件和功能实现

--- maxkb4j-start/src/main/java/com/maxkb4j/start/filter/StaticResourceFilter.java
+++ maxkb4j-start/src/main/java/com/maxkb4j/start/filter/StaticResourceFilter.java
@@ -12,20 +12,19 @@ public class StaticResourceFilter implements Filter {
 
     private static final String REGEX = "^/admin/.*/assets/[\\w.-]+$";
     private static final Pattern PATTERN = Pattern.compile(REGEX);
-    private static final String ADMIN_ASSETS= "/admin/assets";
+    private static final String ADMIN_ASSETS = "/admin/assets";
 
     public static boolean matches(String path) {
-        return PATTERN.matcher(path).matches();
+        return PATTERN.matcher(path).matches() || path.startsWith(ADMIN_ASSETS);
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         String path = httpRequest.getRequestURI();
         if (matches(path)) {
             String assetPath = path.substring(path.lastIndexOf("/"));
-            RequestDispatcher dispatcher = request.getRequestDispatcher(ADMIN_ASSETS+assetPath);
+            RequestDispatcher dispatcher = request.getRequestDispatcher(ADMIN_ASSETS + assetPath);
             dispatcher.forward(request, response);
         } else {
             chain.doFilter(request, response);
