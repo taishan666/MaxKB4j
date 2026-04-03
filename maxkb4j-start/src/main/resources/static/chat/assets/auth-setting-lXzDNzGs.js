17a6479c6afcac9ae39c9e8e5d50fe9a52fe4675
tarzan <1334512682@qq.com>
2026-04-03 20:30:10 +0800

    更新前端资源

---
+++ maxkb4j-start/src/main/resources/static/chat/assets/auth-setting-lXzDNzGs.js
@@ -0,0 +1,1 @@
+import{bm as i,bn as s,bo as r}from"./chat-XycLa1tf.js";const n="/auth",u=(t,o)=>r(`${n}/${t}/detail`,void 0,o),a=(t,o)=>i(`${n}/connection`,t,void 0,o),p=(t,o,e)=>s(`${n}/${t}/info`,o,void 0,e),g={getAuthSetting:u,postAuthSetting:a,putAuthSetting:p};export{g as a};
