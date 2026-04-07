0c27f44d5395c5f9d0e530804f514cefb9045744
tarzan <1334512682@qq.com>
2026-04-07 17:01:22 +0800

    更新前端资源

--- maxkb4j-start/src/main/resources/static/admin/assets/stateDiagram-v2-4FDKWEC3-B10Vt8vA.js
+++
@@ -1,1 +0,0 @@
-import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-Bra4uRZE.js";import{_ as i}from"./admin-mHy3ppcb.js";import"./chunk-55IACEB6-DXnm35G1.js";import"./chunk-QN33PNHL-CvnG3O4X.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
