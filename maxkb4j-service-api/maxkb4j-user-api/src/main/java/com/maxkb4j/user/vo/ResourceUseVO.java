22bbfb5c6101c54b861af1b68292ef4205961a3d
小峰 <172964771@qq.com>
2026-04-07 17:20:05 +0800

    -feat(model):1、增加模型查看资源和知识库的查看资源

---
+++ maxkb4j-service-api/maxkb4j-user-api/src/main/java/com/maxkb4j/user/vo/ResourceUseVO.java
@@ -0,0 +1,22 @@
+package com.maxkb4j.user.vo;
+
+import lombok.Data;
+
+@Data
+public class ResourceUseVO{
+
+    private String id;
+    private String createTime;
+    private String updateTime;
+    private String sourceType;
+    private String targetType;
+    private String sourceId;
+    private String targetId;
+    private String name;
+    private String desc;
+    private String userId;
+    private String workspaceId;
+    private String type;
+    private String folderId;
+    private String username;
+}
---
+++ maxkb4j-service/maxkb4j-system/src/main/java/com/maxkb4j/system/controller/resourceUseController.java
@@ -0,0 +1,33 @@
+package com.maxkb4j.system.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.maxkb4j.common.api.R;
+import com.maxkb4j.common.constant.AppConst;
+import com.maxkb4j.system.service.impl.UserResourcePermissionServiceImpl;
+import com.maxkb4j.user.vo.ResourceUseVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author 小峰
+ * @date 2026-04-05
+ */
+@RestController
+@RequestMapping(AppConst.ADMIN_API + "/workspace/default")
+@RequiredArgsConstructor
+public class resourceUseController {
+    private final UserResourcePermissionServiceImpl resourceUseService;
+
+    @GetMapping("/resource_mapping/MODEL/{resourceId}/{current}/{size}")
+    public R<IPage<ResourceUseVO>> resourceMappingModelPage(@PathVariable String resourceId, @PathVariable int current, @PathVariable int size, String resourceName, String userName, String[] sourceType) {
+        return R.success(resourceUseService.resourceMappingPage(resourceId, current, size, resourceName, userName, sourceType, "MODEL"));
+    }
+
+    @GetMapping("/resource_mapping/KNOWLEDGE/{resourceId}/{current}/{size}")
+    public R<IPage<ResourceUseVO>> resourceMappingKnowledgePage(@PathVariable String resourceId, @PathVariable int current, @PathVariable int size, String resourceName, String userName, String[] sourceType) {
+        return R.success(resourceUseService.resourceMappingPage(resourceId, current, size, resourceName, userName, sourceType, "KNOWLEDGE"));
+    }
+
+}
