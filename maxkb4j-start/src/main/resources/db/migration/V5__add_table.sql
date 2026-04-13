301ca4044513033aba6290e43fbf9f323ffb0798
tarzan <1334512682@qq.com>
2026-04-13 13:43:36 +0800

    fix(database): 修复数据库迁移脚本中的表定义顺序
    - 将 resource_mapping 表的创建脚本从 V5 迁移至 V5 版本文件
    - 删除了原来的 V6__add_table.sql 文件内容
    - 重新创建了包含完整索引和主键约束的 resource_mapping 表结构
    - 确保数据库迁移脚本的一致性和正确执行顺序

--- maxkb4j-start/src/main/resources/db/migration/V5__add_table.sql
+++ maxkb4j-start/src/main/resources/db/migration/V5__add_table.sql
@@ -48,3 +48,25 @@ CREATE INDEX "resource_mapping_update_time" ON "public"."resource_mapping" USING
 -- Primary Key structure for table resource_mapping
 -- ----------------------------
 ALTER TABLE "public"."resource_mapping" ADD CONSTRAINT "resource_mapping_pkey" PRIMARY KEY ("id");
+
+
+-- ----------------------------
+-- Table structure for application_chat_share_link
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."application_chat_share_link";
+CREATE TABLE "public"."application_chat_share_link" (
+                                                        "id" varchar(50) COLLATE "pg_catalog"."default" NOT NULL,
+                                                        "share_type" varchar(25) COLLATE "pg_catalog"."default",
+                                                        "chat_record_ids" varchar[] COLLATE "pg_catalog"."default",
+                                                        "application_id" varchar(50) COLLATE "pg_catalog"."default",
+                                                        "chat_id" varchar(50) COLLATE "pg_catalog"."default",
+                                                        "user_id" varchar(50) COLLATE "pg_catalog"."default",
+                                                        "create_time" timestamptz(6) NOT NULL,
+                                                        "update_time" timestamptz(6) NOT NULL
+)
+;
+
+-- ----------------------------
+-- Primary Key structure for table application_chat_share_link
+-- ----------------------------
+ALTER TABLE "public"."application_chat_share_link" ADD CONSTRAINT "application_chat_share_link_pkey" PRIMARY KEY ("id");
