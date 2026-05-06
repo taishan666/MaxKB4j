c3ac410989c26c8e1155430dcddcbcfc28ef1655
tarzan <1334512682@qq.com>
2026-05-06 18:28:20 +0800

    feat(paragraph): 添加段落元数据支持
    - 引入 com.alibaba.fastjson.JSONObject 依赖
    - 在 ParagraphDTO 中新增 meta 字段用于存储元数据
    - 该字段可用于存储段落相关的附加信息和属性

--- maxkb4j-common/src/main/java/com/maxkb4j/common/domain/dto/ParagraphDTO.java
+++ maxkb4j-common/src/main/java/com/maxkb4j/common/domain/dto/ParagraphDTO.java
@@ -1,5 +1,6 @@
 package com.maxkb4j.common.domain.dto;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.Data;
 
 @Data
@@ -8,6 +9,7 @@ public class ParagraphDTO {
     private String content;
     private Float similarity;
     private String documentName;
+    private JSONObject meta;
     private String knowledgeName;
     private Integer knowledgeType;
 }
