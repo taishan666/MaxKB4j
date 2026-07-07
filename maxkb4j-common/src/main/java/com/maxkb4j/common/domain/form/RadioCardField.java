a4b05e38b68fc8f718860dc186013b5c2e78819e
tarzan <1334512682@qq.com>
2026-07-07 15:21:58 +0800

    refactor(form): 移除 RadioCardField 构造函数中的冗余参数
    - 删除了带有默认值参数的构造函数
    - 简化了构造函数实现，统一使用无默认值的构造方式
    - 保持了原有的输入类型设置逻辑

--- maxkb4j-common/src/main/java/com/maxkb4j/common/domain/form/RadioCardField.java
+++ maxkb4j-common/src/main/java/com/maxkb4j/common/domain/form/RadioCardField.java
@@ -4,11 +4,6 @@ import java.util.Map;
 
 public class RadioCardField extends SingleSelectField {
 
-    public RadioCardField(String labelName, String field, Map<String,Object> options, Object defaultValue) {
-        super(labelName,field,"",options,defaultValue);
-        super.setInput_type("RadioCard");
-    }
-
     public RadioCardField(String labelName, String field, Map<String,Object> options) {
         super(labelName,field,"",options,null);
         super.setInput_type("RadioCard");
