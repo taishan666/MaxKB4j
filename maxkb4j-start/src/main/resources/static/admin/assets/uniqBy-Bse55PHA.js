cbd3f5bb373401469b801698bec6f4c440eda1df
tarzan <1334512682@qq.com>
2026-07-10 18:23:17 +0800

    前端样式修改

--- maxkb4j-start/src/main/resources/static/admin/assets/uniqBy-Bse55PHA.js
+++
@@ -1,1 +0,0 @@
-import{au as n,av as s}from"./admin-BWln8uwL.js";function a(e,t){return e&&e.length?n(e,s(t)):[]}export{a as u};
--- maxkb4j-start/src/main/resources/static/chat/assets/channel-q3ho37ja.js
+++
@@ -1,1 +0,0 @@
-import{ar as o,as as s}from"./chat-C3Tel3JI.js";const t=(r,a)=>o.lang.round(s.parse(r)[a]);export{t as c};
